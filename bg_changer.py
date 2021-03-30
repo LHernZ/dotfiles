3aaf4669f5c41f3e3534eb64a0f9c0e3e3ad62e0
luishernandezIMT <luisfernando.imt@gmail.com>
2021-03-30 11:02:49 -0600

    Clean script, pointing full paths

--- bg_changer.py
+++ bg_changer.py
@@ -1,18 +1,18 @@
 import random, os
 import json
 import sys
-path = "bgs"
+path = "/mnt/c/Users/luisf/OneDrive/Terminal"
 
 random_filename = random.choice([
     x for x in os.listdir(path)
     if os.path.isfile(os.path.join(path, x))
 ])
 
-with open('settings.json', 'r') as f:
+with open('/mnt/c/Users/luisf/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json', 'r') as f:
     data = json.load(f)
 
 t_path, f_name = str(data["profiles"]["defaults"]["backgroundImage"]).rsplit("\\",1)
-data["profiles"]["defaults"]["backgroundImage"] = t_path + "\\" + str(sys.argv[1])[2:]
+data["profiles"]["defaults"]["backgroundImage"] = t_path + "\\" + str(sys.argv[1])
 
 with open('settings.json', 'w') as f:
     json.dump(data, f)
